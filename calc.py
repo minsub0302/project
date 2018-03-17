41445c0e6f727bfed1bb15808818e1b57a45b3ae
DESKTOP-C5HKG4R\minsu <leeminsub0302@gmail.com>
2018-03-17 21:50:02 +0900

    문제

--- calc.py
+++ calc.py
@@ -1,1 +1,17 @@
+import random as r
 
+def calc():
+	a = r.randint(1,100)
+	b = r.randint(1,100)
+	c = r.randint(1,3)
+	if c == 1:
+		k = '+'
+	elif c == 2:
+		k = '-'
+	elif c == 3:
+		k = '*'
+	
+	I = str(a)+k+str(b)
+	return I
+		
+print(calc())
